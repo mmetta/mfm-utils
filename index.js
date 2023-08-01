05162a4038b8f7d807e848ae1de8d268d0e43915
mmetta <42844751+mmetta@users.noreply.github.com>
2023-08-01 15:21:59 -0300

    Add format CPF

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 function formatCep(value) {
-    return String(value)
-        .replace(/\D/g, "")
-        .slice(0, 8)
-        .replace(/(\d{5})(\d)/, "$1-$2")
-
+    try {
+        ep = String(value).replace(/\D/g, "").slice(0, 8)
+        if (ep.length !== 8) {
+            return null
+        }
+        cep = ep.replace(/(\d{5})(\d)/, "$1-$2")
+        return cep
+    } catch (error) {
+        console.log(error)
+        return null
+    }
 }
 
+
+function formatCpf(value) {
+    try {
+        cpf = String(value).replace(/\D/g, "").slice(0, 11)
+        if (cpf.length !== 11) {
+            return null
+        }
+        cpf = cpf.replace(/(\d{3})(\d{3})(\d{3})(\d)/, "$1.$2.$3-$4")
+        return cpf
+    } catch (error) {
+        console.log(error)
+        return null
+    }
+}
+
+
 module.exports = {
-    formatCep
+    formatCep, formatCpf
 }
